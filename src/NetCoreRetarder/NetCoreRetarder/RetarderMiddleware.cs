70204517bbf21d8b39e71016bcdfb86bb97b470c
ignatandrei <ignatandrei@yahoo.com>
2020-04-09 08:00:25 +0300

    First version

---
+++ src/NetCoreRetarder/NetCoreRetarder/RetarderMiddleware.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetCoreRetarder
+{
+    public class RetarderMiddleware : IMiddleware
+    {
+
+        public RetarderMiddleware()
+        {
+        }
+
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            var random = new Random();
+            //var awaitMilliseconds =random.Next(10000, 100000);
+            var awaitMilliseconds = random.Next(1, 1000);
+            Console.WriteLine($"awaiting {awaitMilliseconds}");
+            await Task.Delay(awaitMilliseconds);
+            Console.WriteLine($"***awaited {awaitMilliseconds}");
+
+            await next(context);
+        }
+    }
+}
